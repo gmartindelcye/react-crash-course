d64fd77dec9fb8a9c4263c4e0c869f93323b6d28
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-01-26 20:56:19 -0600

    props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 
 const App =() => {
 
-  const Person = () => {
+  const Person = (props) => {
     return (
       <>
-        <h1>Name: John</h1>
-        <h2>Last Name: Doe</h2>
-        <h2>Age: 25</h2>
+        <h1>Name: {props.name}</h1>
+        <h2>Last Name: {props.lastName}</h2>
+        <h2>Age: {props.age}</h2>
       </>
     )
   }
   
   return (
     <div className="App">
-      <Person />
+      <Person name="John" lastName="Doe" age="25" />
+      <Person name="Jane" lastName="Doe" age="30" />
     </div>
   );
 }
